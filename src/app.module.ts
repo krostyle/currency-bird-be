4450ed93492a02bb90249e790f763dc902b9c7f8
Krostyle <diegoo.albarran@gmail.com>
2022-05-17 18:59:52 -0400

    delete users crud , add roles controller, module and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
-import { UsersModule } from './users/users.module';
+import { RolesModule } from './roles/roles.module';
+
 
 @Module({
-  imports: [UsersModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [RolesModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
